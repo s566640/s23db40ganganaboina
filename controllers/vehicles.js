4f22cb5d93cabfdbd607eff5346f2cc0d831d9d9
s566640 <s566640@nwmissouri.edu>
2023-11-15 22:03:58 -0600

    updated controllers

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -46,12 +46,12 @@ exports.vehicles_create_post = async function (req, res) {
 exports.vehicles_delete = async function(req, res) {
     console.log("delete " + req.params.id)
     try {
-    result = await vehicles.findByIdAndDelete(req.params.id)
-    console.log("Removed " + result)
-    res.send(result)
-    } catch (err) {
-    res.status(500)
-    res.send(`{"error": Error deleting ${err}}`);
+        result = await vehicles.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    }   catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
     }
     };
     
